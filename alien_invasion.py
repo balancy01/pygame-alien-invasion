1d0a10e4b2b3c3c220b5f082bfa2f42955875965
Yuriy_Galkin <ura.galkin@gmail.com>
2023-05-04 17:17:37 +0700

    Scoring: Displaying the Score

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ import pygame
 
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
 from button import Button
 from ship import Ship
 from bullet import Bullet
@@ -24,7 +25,9 @@ class AlienInvation():
 		pygame.display.set_caption("Alien Invasion")
 
 		#Создание экземпляра для хранения игровой статистики
+		#и панели результатов
 		self.stats = GameStats(self)
+		self.sb = Scoreboard(self)
 
 		self.ship = Ship(self)
 		self.bullets = pygame.sprite.Group()
@@ -223,6 +226,9 @@ class AlienInvation():
 		for bullet in self.bullets.sprites():
 			bullet.draw_bullet()
 		self.aliens.draw(self.screen)
+
+		#Вывод информации о счете
+		self.sb.show_score()
 
 		#Кнопка Play отображается в том случае, если игра неактивна
 		if not self.stats.game_active:
